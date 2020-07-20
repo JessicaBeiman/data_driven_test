8c633add77b28a10c4a8f4dbdeeaad55f97e1f56
Jessica Zhu <jessica.zhu.contractor@pepsico.com>
2020-07-20 15:16:09 +0800

    Package 'find element' function

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ A project of data driven automation test practice.
 Step by step to build a data-driven automation test framework following the reference from: https://www.cnblogs.com/xiaxiaoxu/p/9297298.html  
 What this project does is to login Salesforce and create an account.  
 
-步骤1：搭建。按过程罗列代码实现功能。  
-步骤2：封装登录。
+步骤1：搭建。  
+创建test主程序，按过程罗列代码实现功能。  
+步骤2：封装登录。  
+新创建一个LoginPage类，在这个类下面创建一个login函数实现登录功能。将test主程序里面登录这一部分的代码移到LoginPage类下的login函数里面。在test主程序里面调用LoginPage类的login方法。  
+步骤3：封装创建Account.  
+新创建一个Account类，在这个类下面创建一个new_account函数实现新建account功能。将test主程序里面新建account这一部分的代码移到Account类下的new_account函数里面。在test主程序里面调用Account类的new_account方法。  
+步骤4：封装查找元素的功能。  
+将每一个元素的查找单独封装成一个方法。在登录过程中的每一步调用每个元素的查找方法并传递参数。
--- page_object/login_page.py
+++ page_object/login_page.py
@@ -12,26 +12,19 @@ import traceback
 class LoginPage(object):
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(self.driver, 10, 0.2)  # 显示等待
 
-    def login(self):
-        try:
-            wait = WebDriverWait(self.driver, 10, 0.2)  # 显示等待
-            username = wait.until(lambda x: x.find_element_by_xpath('//*[@id="username"]'))
-            username.send_keys('jessica.test@dev.com')
-            password = wait.until(lambda x: x.find_element_by_xpath('//*[@id="password"]'))
-            password.send_keys('passwordtest')
-            submit = wait.until(lambda x: x.find_element_by_xpath('//*[@id="Login"]'))
-            submit.click()
-            self.driver.switch_to.default_content()
-            time.sleep(20)
-            assert 'Home' in self.driver.page_source, '"Home" not exist in page_source.'
+    def get_username(self, locate_type, locate_expression):
+        username = self.wait.until(lambda x: x.find_element(by=locate_type, value=locate_expression))
+        return username
 
-        except TimeoutException as e:
-            print(traceback.print_exc())
-        except NoSuchElementException as e:
-            print(traceback.print_exc())
-        except Exception as e:
-            print(traceback.print_exc())
+    def get_password(self, locate_type, locate_expression):
+        password = self.wait.until(lambda x: x.find_element(by=locate_type, value=locate_expression))
+        return password
+
+    def get_login_button(self, locate_type, locate_expression):
+        submit = self.wait.until(lambda x: x.find_element(by=locate_type, value=locate_expression))
+        return submit
 
 
 if __name__ == '__main__':
@@ -41,5 +34,10 @@ if __name__ == '__main__':
                               executable_path='C:\\PycharmProjects\\data_driven_test\\chromedriver.exe')
     driver.get('https://ap8.salesforce.com/')
     driver.maximize_window()
-    login = LoginPage(driver)
-    login.login()
+    lp = LoginPage(driver)
+    lp.get_username('xpath', '//*[@id="username"]').send_keys('jessica.test@dev.com')
+    lp.get_password('xpath', '//*[@id="password"]').send_keys('passwordtest')
+    lp.get_login_button('xpath', '//*[@id="Login"]').click()
+    driver.switch_to.default_content()
+    time.sleep(20)
+    assert 'Home' in driver.page_source, '"Home" not exist in page_source.'
